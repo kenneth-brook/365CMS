68e8d9cd804c0d34bfd653d0a8c5ccef47106f65
Kenneth Brook <kenneth.brook@hotmail.com>
2024-04-12 15:32:23 -0400

    converted for lambda use

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const express = require('express');
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('express')();
 const path = require('path');
-const app = express();
 
-// Define the port to listen on
-const PORT = process.env.PORT || 3000;
-
-// Serve static files from the 'static' directory within '365cms'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Optionally, define a route for "/" to specifically serve index.html from the 'public' directory
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Handle any other paths with a 404 response
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Instead of app.listen
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context); 
