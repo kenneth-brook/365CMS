db0cf52dccebafb0a7d34a789825338e555b19f7
Kenneth Brook <kenneth.brook@hotmail.com>
2024-04-15 14:19:23 -0400

    preped staged ready for db

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,14 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 
 const app = express();
 
-// Use the cors middleware to enable CORS
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('Connection good');
 });
 
-// Instead of app.listen
 const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
-
--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const portInfoElement = document.getElementById('portInfo');
-
-fetch('/')
-    .then(response => response.text())
+document.addEventListener("DOMContentLoaded", function () {
+    // Make API call to fetch server port information
+    fetch('https://p70bluj1kf.execute-api.us-east-1.amazonaws.com/')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch server port information');
+        }
+        return response.text();
+    })
     .then(data => {
-        portInfoElement.textContent = `The server responded: ${data}`;
+        // Update the content of the h1 element with the response data
+        document.getElementById('portInfo').innerHTML = data;
     })
     .catch(error => {
-        console.error('Error fetching port information:', error);
-        portInfoElement.textContent = 'Failed to load port information.';
+        // Handle errors
+        console.error('Error:', error);
+        document.getElementById('portInfo').innerHTML = 'Error: ' + error.message;
     });
 });
