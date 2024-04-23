23b54f4247228c88adeb930f61048191b7bcebd0
Kenneth Brook <kenneth.brook@hotmail.com>
2024-04-23 16:18:16 -0400

    back to standard for test

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,28 +10,12 @@ app.use(cors({
     //allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.get('/setup', async (req, res) => {
-    console.log("Received request at /setup");
-    try {
-        setupRoutes;
-        res.status(200).json({ message: 'Setup completed successfully' });
-    } catch (error) {
-        console.error('Setup failed:', error);
-        res.status(500).json({ message: 'Setup failed', error: error.message });
-    }
-});
+app.use('/setup', setupRoutes);
 
-//app.use('/setup', setupRoutes);
-
-app.get('/', (req, res) => {
-    console.log('Handling root');
-    res.send('Root route');
-});
+app.use('/', stateManagementRoutes);
 
 app.get('*', (req, res) => {
     res.status(200).send(`You hit path: ${req.path}`);
 });
-
-//app.use('/', stateManagementRoutes);
 
 module.exports = app;
