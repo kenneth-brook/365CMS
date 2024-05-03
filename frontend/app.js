d0bd8a96f107cd2d03b0d366fa32bfa5d788e073
Kenneth Brook <kenneth.brook@hotmail.com>
2024-05-03 16:24:34 -0400

    solid start on login

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Make API call to fetch server port information
-    fetch('https://p70bluj1kf.execute-api.us-east-1.amazonaws.com/')
+    fetch('https://8pz5kzj96d.execute-api.us-east-1.amazonaws.com/Prod/')
     .then(response => {
         if (!response.ok) {
             throw new Error('Failed to fetch server port information');
@@ -8,11 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
         return response.text();
     })
     .then(data => {
-        // Update the content of the h1 element with the response data
         document.getElementById('portInfo').innerHTML = data;
     })
     .catch(error => {
-        // Handle errors
         console.error('Error:', error);
         document.getElementById('portInfo').innerHTML = 'Error: ' + error.message;
     });
