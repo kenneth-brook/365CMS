47c910c0ce05526b7a4495c3bc73ff411374aa3e
Kenneth Brook <kenneth.brook@hotmail.com>
2024-06-20 15:52:10 -0400

    corrected play route

--- backend/src/routes/playFormRoutes.js
+++ backend/src/routes/playFormRoutes.js
@@ -9,17 +9,13 @@ router.post('/', async (req, res) => {
   const pool = await getDbPool();
   const client = await pool.connect();
   try {
-    const { businessId, menuTypes, special_days } = req.body;
+    const { businessId, menuTypes, special_days, hours } = req.body;
 
-    // Debugging logs to check the received data
-    console.log('businessId:', businessId);
-    console.log('menuTypes:', menuTypes);
-    console.log('special_days:', special_days);
 
-    // Insert into eat table
+    // Insert into play table
     const eatResult = await client.query(
-      'INSERT INTO play (business_id, play_types, special_days) VALUES ($1, $2, $3) RETURNING id',
-      [businessId, JSON.stringify(JSON.parse(menuTypes)), JSON.stringify(JSON.parse(special_days))]
+      'INSERT INTO play (business_id, play_types, special_days, hours) VALUES ($1, $2, $3, $4) RETURNING id',
+      [businessId, JSON.stringify(JSON.parse(menuTypes)), JSON.stringify(JSON.parse(special_days)), JSON.stringify(JSON.parse(hours))]
     );
     const eatId = eatResult.rows[0].id;
 
